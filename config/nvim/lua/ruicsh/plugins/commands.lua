9e9c297e9d6527b8a45e43b9427b2dd2717cb618
Rui Costa <ruicosta042@gmail.com>
2024-10-13 12:40:51 +0100

    fix(nvim): pick auto-cmp with enter

--- config/nvim/lua/ruicsh/plugins/commands.lua
+++ config/nvim/lua/ruicsh/plugins/commands.lua
@@ -34,6 +34,7 @@ return {
 					--  This will auto-import if your LSP supports it.
 					--  This will expand snippets if the LSP sent a snippet.
 					["<tab>"] = cmp.mapping.confirm({ select = true }),
+					["<enter>"] = cmp.mapping.confirm({ select = true }),
 
 					-- Manually trigger a completion from nvim-cmp.
 					--  Generally you don't need this, because nvim-cmp will display
