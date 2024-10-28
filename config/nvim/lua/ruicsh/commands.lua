e7519c28bd891fc80feaa5eaa849530dca133bcc
Rui Costa <ruicosta042@gmail.com>
2024-10-28 19:10:59 +0000

    feat(nvim): start terminal in auto mode

--- config/nvim/lua/ruicsh/commands.lua
+++ config/nvim/lua/ruicsh/commands.lua
@@ -1,8 +1,13 @@
--- don't show line numbers on terminal
+-- when starting a terminal
 vim.api.nvim_create_autocmd("TermOpen", {
 	pattern = "*",
 	callback = function()
-		vim.opt_local.number = false
-		vim.opt_local.relativenumber = false
+		if vim.opt.buftype:get() == "terminal" then
+			-- don't show line numbers on terminal
+			vim.opt_local.number = false
+			vim.opt_local.relativenumber = false
+			-- start on insert mode
+			vim.cmd(":startinsert")
+		end
 	end,
 })
