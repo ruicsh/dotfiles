4dd5730dfa9f5f300a3de0c482a0558e2957d051
Rui Costa <ruicosta042@gmail.com>
2024-10-29 15:04:47 +0000

    fix(nvim): barbecue theme

--- config/nvim/lua/ruicsh/plugins/barbecue.lua
+++ config/nvim/lua/ruicsh/plugins/barbecue.lua
@@ -1,17 +1,23 @@
 -- breadcrumbs
 -- https://github.com/utilyre/barbecue.nvim
 
+local c = require("ruicsh.theme.colors")
+
 return {
 	"utilyre/barbecue.nvim",
 	name = "barbecue",
 	opts = {
-		-- configurations go here
+		theme = {
+			normal = { fg = c.nord4 },
+			ellipsis = { fg = c.nord4 },
+			separator = { fg = c.nord4 },
+			dirname = { fg = c.nord4 },
+		},
 	},
 
 	version = "*",
 	dependencies = {
 		"SmiteshP/nvim-navic",
-		"nvim-tree/nvim-web-devicons", -- optional dependency
+		"nvim-tree/nvim-web-devicons",
 	},
 }
-
