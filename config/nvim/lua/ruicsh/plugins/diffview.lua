5503024d8afb97c1bd13e2fdfff75e99f83648ce
Rui Costa <ruicosta042@gmail.com>
2024-10-30 03:07:03 +0000

    fix(nvim): use cc to commit from diffview

--- config/nvim/lua/ruicsh/plugins/diffview.lua
+++ config/nvim/lua/ruicsh/plugins/diffview.lua
@@ -1,3 +1,6 @@
+-- diffview
+-- https://github.com/sindrets/diffview.nvim
+
 return {
 	"sindrets/diffview.nvim",
 	keys = {
@@ -17,12 +20,14 @@ return {
 			view_enter = function()
 				vim.cmd(":SunglassesDisable") -- unshade all windows
 				vim.cmd(":Barbecue hide") -- hide breadcrumbs
-				vim.keymap.set("n", "<c-q>", "<cmd>DiffviewClose<cr>") -- use <c-q> to close
+
+				-- keymaps set only for this buffer
+				vim.keymap.set("n", "<c-q>", "<cmd>DiffviewClose<cr>", { buffer = 0 }) -- use <c-q> to close,
+				vim.keymap.set("n", "cc", "<cmd>DiffviewClose<cr><cmd>vertical Git commit<cr>", { buffer = 0 }) -- use cc to commit
 			end,
 			view_leave = function()
 				vim.cmd(":SunglassesEnable") -- bring back the shades
 				vim.cmd(":Barbecue show") -- show breadcrumbs
-				vim.keymap.set("n", "<c-q>", "<c-w>q") -- bring back the default keymap
 			end,
 		},
 	},
