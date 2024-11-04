2d242564e15cc658f9b96f2e465f139b02d85921
Rui Costa <ruicosta042@gmail.com>
2024-11-04 14:47:26 +0000

    feat(nvim): add <c-v> to paste on insert mode

--- config/nvim/lua/ruicsh/keymaps/insert.lua
+++ config/nvim/lua/ruicsh/keymaps/insert.lua
@@ -1,5 +1,8 @@
 local k = vim.keymap.set
 
+-- paste
+k("i", "<c-v>", "<c-r>", { desc = "Paste" })
+
 -- navigation (alt-* keymaps from vim-rsi don't work on vscode)
 k("i", "<c-j>", "<c-o>j", { desc = "Jump down a line" })
 k("i", "<c-k>", "<c-o>k", { desc = "Jump up a line" })
