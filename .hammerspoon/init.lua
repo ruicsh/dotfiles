0d5fc37d5f8b3c2974c38a17708ad29b8eff6bd8
Rui Costa <ruicosta042@gmail.com>
2024-12-28 20:16:30 +0000

    refactor(hammerspoon): format lua code

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,6 +1,4 @@
+Hyper = { "ctrl", "alt", "shift", "cmd" }
 
-hyper = {"ctrl", "alt", "shift", "cmd"}
-
-hs.loadSpoon("AppLauncher")
 hs.loadSpoon("ReloadConfiguration")
 hs.loadSpoon("WindowManager")
