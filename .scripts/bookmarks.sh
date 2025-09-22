819667bce02a324b31ee46e1fff073018f234add
Rui Costa <ruicosta042@gmail.com>
2025-09-22 17:08:11 +0100

    fix(bookmarks) Detect Windows and WSL

--- .scripts/bookmarks.sh
+++ .scripts/bookmarks.sh
@@ -25,8 +25,18 @@ name=$(echo "$selection" | awk -F'•' '{print $1}' | xargs)
 url=$(awk -F, -v nm="$name" '{gsub(/"/, "", $1); gsub(/"/, "", $2); if ($1 == nm) print $2}' "$BOOKMARKS_FILE")
 url=$(echo "$url" | sed -E 's/^"(.*)"$/\1/')
 
+is_windows_wsl=false
+is_windows=false
 if grep -qEi "(Microsoft|WSL)" /proc/version &> /dev/null; then
+  is_windows_wsl=true
+elif [[ "$OS" == "Windows_NT" ]] || [[ -n "$WINDIR" ]] || command -v cmd.exe &> /dev/null 2>&1; then
+  is_windows=true
+fi
+
+if $is_windows_wsl; then
   cmd.exe /c "pushd C:\Windows && start msedge.exe $url && popd" > /dev/null 2>&1
+elif $is_windows; then
+  start "$url" 2>/dev/null
 else
   open "$url"
 fi
